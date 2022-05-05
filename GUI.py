dce0287a542d2e3b707d9fc1fd34d1c63356d5d4
AUSTIN HILDERBRAND <rts.things@gmail.com>
2022-05-04 23:55:58 -0600

    removed comments from GUI.py

--- GUI.py
+++ GUI.py
@@ -2,12 +2,8 @@
 Power Logger GUI
 Team B - Radiation Test Solutions
 ECEN 499 Senior Project
-Author: Austin Hilderbrand
 """
 
 # %%
 # GUI lib
 import tkinter
-
-# %%
-# TODO: tkinter tutorial code
