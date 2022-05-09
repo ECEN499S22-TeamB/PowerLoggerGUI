24e1c04c6f9fd481931a5b7e5d5be9d3aafe4e05
AUSTIN HILDERBRAND <rts.things@gmail.com>
2022-05-09 12:03:24 -0600

    added contact info to header of GUI.py and added some preliminary structure

--- GUI.py
+++ GUI.py
@@ -1,9 +1,41 @@
 """
 Power Logger GUI
-Team B - Radiation Test Solutions
 ECEN 499 Senior Project
+Instructor: Kevin Smith
+    208-496-7612
+    smithk@byui.edu
+
+Team B - Radiation Test Solutions
+    Aaron Mears (team lead)
+        509-596-8539
+        amears@radiationtestsolutions.com
+    Austin Hilderbrand
+        602-390-5594
+        hil16039@byui.edu
+    Nathan Taylor
+        775-397-8139
+        tay18060@byui.edu
+    Joshua Armstrong
+        208-406-8689
+        arm05006@byui.edu
 """
 
-# %%
-# GUI lib
-import tkinter
+# Setup visuals --------------------------------------------------------------
+# Import Tkinter
+import tkinter as tk
+
+# Create a new window
+window = tk.Tk()
+
+# TODO: setup remaining visuals
+# ----------------------------------------------------------------------------
+
+
+# Wait for user input --------------------------------------------------------
+window.mainloop()   # CRITICAL: script won't work without this
+# ----------------------------------------------------------------------------
+
+
+# Functions for interactive widgets ------------------------------------------
+# TODO: add functions for interactive widgets?
+# ----------------------------------------------------------------------------
