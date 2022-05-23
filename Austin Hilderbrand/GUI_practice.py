a18aaf963dc38ba4ff6b646b280562751f18b428
AUSTIN HILDERBRAND <rts.things@gmail.com>
2022-05-23 11:10:01 -0600

    completed tkinter tutorial

--- Austin Hilderbrand/GUI_practice.py
+++ Austin Hilderbrand/GUI_practice.py
@@ -489,14 +489,108 @@ Austin Hilderbrand
 # ---------------------------------------------------------------------------
 # ----------------- Building a Temperature Converter ------------------------
 # ---------------------------------------------------------------------------
-# Imports
+# # =============== Imports
+# import tkinter as tk
+
+# # =============== Event handlers
+# def fahrenheit_to_celsius():
+#     """Convert the value for Fahrenheit to Celsius and insert the result
+#     into lbl_result.
+#     """
+#     fahrenheit = ent_temperature.get()
+#     celsius = (5/9) * (float(fahrenheit) - 32)
+#     lbl_result["text"] = f"{round(celsius, 2)} \N{DEGREE CELSIUS}"
+
+# # =============== Setup visuals
+# # Window
+# window = tk.Tk()
+# window.title("Temperature Converter")
+# window.resizable(width=False, height=False)
+
+# # Temperature entry
+# frm_entry = tk.Frame(master=window)
+# ent_temperature = tk.Entry(master=frm_entry, width=10)
+# lbl_temp = tk.Label(master=frm_entry, text="\N{DEGREE FAHRENHEIT}")
+
+# # Temperature entry placement (within frm_entry frame)
+# ent_temperature.grid(row=0, column=0, sticky="e")
+# lbl_temp.grid(row=0, column=1, sticky="w")
+
+# # Temperature conversion button
+# btn_convert = tk.Button(
+#     master=window,
+#     text="\N{RIGHTWARDS BLACK ARROW}",
+#     command=fahrenheit_to_celsius
+# )
+
+# # Conversion result field
+# lbl_result = tk.Label(master=window, text="\N{DEGREE CELSIUS}")
+
+# # Place everything in window
+# frm_entry.grid(row=0, column=0, padx=10)
+# btn_convert.grid(row=0, column=1, pady=10)
+# lbl_result.grid(row=0, column=2, padx=10)
+
+# # =============== Run the event loop
+# window.mainloop()
+
+# ---------------------------------------------------------------------------
+# ---------------------- Building a Text Editor -----------------------------
+# ---------------------------------------------------------------------------
+# ============= Imports
 import tkinter as tk
+from tkinter.filedialog import askopenfilename
+from tkinter.filedialog import asksaveasfilename
 
-# Event handlers
-# TODO: add functons
+# ============= Event handlers
+def open_file():
+    """Open a file for editing."""
+    filepath = askopenfilename(
+        filetypes=[('Text Files', "*.txt"), ("All Files", "*.*")]
+    )
+    if not filepath:
+        return
+    txt_edit.delete("1.0", tk.END)
+    with open(filepath, mode="r", encoding="utf-8") as input_file:
+        text = input_file.read()
+        txt_edit.insert(tk.END, text)
+    window.title(f"Simple Text Editor - {filepath}")
 
-# Setup visuals
+def save_file():
+    """Save the current file as a new file."""
+    filepath = asksaveasfilename(
+        defaultextension=".txt",
+        filetypes=[("Text Files", "*.txt"), ("All Files", "*.*")]
+    )
+    if not filepath:
+        return
+    with open(filepath, mode="w", encoding="utf-8") as output_file:
+        text = txt_edit.get("1.0", tk.END)
+        output_file.write(text)
+    window.title(f"Simple Text Editor - {filepath}")
+
+# ============= Setup visuals
+# Create new window
 window = tk.Tk()
-window.title("Temperature Converter")
-window.resizable(width=False, height=False)
+window.title("Simple Text Editor")
 
+# Configure the window
+window.rowconfigure(0, minsize=800, weight=1)
+window.columnconfigure(1, minsize=800, weight=1)
+
+# Create widgets
+txt_edit = tk.Text(window)
+frm_buttons = tk.Frame(window, relief=tk.RAISED, bd=2)
+btn_open = tk.Button(frm_buttons, text="Open", command=open_file)
+btn_save = tk.Button(frm_buttons, text="Save as...", command=save_file)
+
+# Assign buttons to frame
+btn_open.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
+btn_save.grid(row=1, column=0, sticky="ew", padx=5)
+
+# Setup window grid layout
+frm_buttons.grid(row=0, column=0, sticky="ns")
+txt_edit.grid(row=0, column=1, sticky="nsew")
+
+# ============= Run the event loop
+window.mainloop()
