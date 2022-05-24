4b3886415f84d8a8b69d3d632ed690d99bed5b3f
AUSTIN HILDERBRAND <rts.things@gmail.com>
2022-05-23 18:06:52 -0600

    device_disc can now select a user-specified device to read from started some file to learn about running multiple processes simultaneously

---
+++ Austin Hilderbrand/USB_HWID.py
@@ -0,0 +1,16 @@
+"""
+USB_HWID.py
+My research into USB connection and HWID device info
+Austin Hilderbrand
+NOTE: pyusb tutorial --> https://github.com/pyusb/pyusb/blob/master/docs/tutorial.rst
+"""
+
+#!/usr/bin/python
+import sys
+import usb.core
+# find USB devices
+dev = usb.core.find(find_all=True)
+# loop through devices, printing vendor and product ids in decimal and hex
+for cfg in dev:
+  sys.stdout.write('Decimal VendorID=' + str(cfg.idVendor) + ' & ProductID=' + str(cfg.idProduct) + '\n')
+  sys.stdout.write('Hexadecimal VendorID=' + hex(cfg.idVendor) + ' & ProductID=' + hex(cfg.idProduct) + '\n\n')
