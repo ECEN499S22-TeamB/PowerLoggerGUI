9a4e984c5529c2eedf231dab217b41b9fa5ccc99
AUSTIN HILDERBRAND <rts.things@gmail.com>
2022-05-30 22:30:59 -0600

    formatting work on device_disc.py

--- Austin Hilderbrand/device_disc.py
+++ Austin Hilderbrand/device_disc.py
@@ -26,8 +26,6 @@ The DI-1100 protocol this program uses can be downloaded from the instrument's
 product page:
 https://www.dataq.com/resources/pdfs/misc/di-1100-protocol.pdf
 """
-
-
 import serial
 import serial.tools.list_ports
 import keyboard
@@ -65,16 +63,12 @@ achan_accumulation_table = list(())
 acquiring = False
 
 
-
-
-
-
-
-
-# ------------------------------------------------------------------------------------
-""" Discover DATAQ Instruments devices and models.  Note that if multiple devices are connected, only the 
-device discovered first is used. We leave it to you to ensure that it's a DI-1100."""
+# ============================================================================
+# discovery
+# ============================================================================
 def discovery():
+    """ Discover DATAQ Instruments devices and models.  Note that if multiple devices are connected, only the 
+    device discovered first is used. We leave it to you to ensure that it's a DI-1100."""
     # Get a list of active com ports to scan for possible DATAQ Instruments devices
     available_ports = list(serial.tools.list_ports.comports())
     # Will eventually hold the com port of the detected device, if any
@@ -114,21 +108,13 @@ def discovery():
         print("Please connect a DATAQ Instruments device")
         input("Press ENTER to try again...")
         return(False)
-# --------------------------------------------------------------------------------------
 
 
-
-
-
-
-
-
-
-
-
-
-# Sends a passed command string after appending <cr>
+# ============================================================================
+# send_cmd
+# ============================================================================
 def send_cmd(command):
+    """Sends a passed command string after appending <cr>"""
     ser.write((command+'\r').encode())
     time.sleep(.1)
     if not(acquiring):
@@ -148,9 +134,12 @@ def send_cmd(command):
                     print (s)
                     break
 
-# Configure the instrment's scan list
-def config_scn_lst():
-    # Scan list position must start with 0 and increment sequentially
+
+# ============================================================================
+# config_scn_lst
+# ============================================================================
+def config_scn_lst():    # Scan list position must start with 0 and increment sequentially
+    """Configure the instrument's scan list"""
     position = 0
     for item in slist:
         send_cmd("slist "+ str(position ) + " " + str(item))
@@ -158,6 +147,10 @@ def config_scn_lst():
         achan_accumulation_table.append(0)
         position += 1
 
+
+# ----------------------------------------------------------------------------
+# Setup hooked DATAQ unit prior to collecting readings
+# ----------------------------------------------------------------------------
 while discovery() == False:
     discovery()
 # Stop in case DI-1100 is already scanning
@@ -190,7 +183,11 @@ achan_number = 0
 # This is the constructed output string
 output_string = ""
 
-# This is the main program loop, broken only by typing a command key as defined
+
+# ----------------------------------------------------------------------------
+# This is the main program loop, broken only by typing a command key as 
+#   defined
+# ----------------------------------------------------------------------------
 while True:
     # If key 'G' start scanning
     if keyboard.is_pressed('g' or  'G'):
