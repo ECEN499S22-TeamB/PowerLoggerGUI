711be37fc193586af6b7f0ad90c81d1eab62f9cf
AUSTIN HILDERBRAND <rts.things@gmail.com>
2022-06-03 22:38:15 -0600

    read_DATAQs.py now reads and outputs data from multiple DATAQs. It accomplishes this with a single running process. The code still needs some additional polishing/comments.

--- Austin Hilderbrand/read_DATAQs.py
+++ Austin Hilderbrand/read_DATAQs.py
@@ -54,7 +54,8 @@ every nth value, but is recommended since it reduces noise by a factor of n^0.5
 'decimation_factor' = 1 disables decimation and attemps to output all values.
 """
 # Define a decimation factor variable
-decimation_factor = 1000
+# decimation_factor = 1000
+decimation_factor = 250
 
 # Contains accumulated values for each analog channel used for the average calculation
 # achan_accumulation_table = list(())
@@ -194,13 +195,13 @@ for ser_port in range(len(ser_ports)):
 
     # This is the slist position pointer. Ranges from 0 (first position)
     # to len(slist)
-    slist_pointer = 0
+    slist_pointers = [0] * len(ser_ports)
 
     # Init a decimation counter:
-    dec_count = decimation_factor
+    dec_counts = [decimation_factor] * len(ser_ports)
 
     # Init the logical channel number for enabled analog channels
-    achan_number = 0
+    achan_numbers = [0] * len(ser_ports)
 
     # This is the constructed output string
     # output_string = ""
@@ -246,7 +247,7 @@ while True:
                 result = int.from_bytes(bytes,byteorder='little', signed=True)
 
                 # Since digital input states are embedded into the analog data stream there are four possibilities:
-                if (dec_count == 1) and (slist_pointer == 0):
+                if (dec_counts[ser_port] == 1) and (slist_pointers[ser_port] == 0):
                     # Decimation loop finished and first slist position
                     # Two LSBs carry information only for first slist posiiton. So, ...
                     # Preserve lower two bits representing digital input states
@@ -255,56 +256,58 @@ while True:
                     result = result >> 2
                     result = result << 2
                     # Add the value to the accumulator
-                    achan_accumulation_tables[ser_port][achan_number] = result + achan_accumulation_tables[ser_port][achan_number]
-                    achan_number += 1
+                    achan_accumulation_tables[ser_port][achan_numbers[ser_port]] = result + achan_accumulation_tables[ser_port][achan_numbers[ser_port]]
+                    achan_numbers[ser_port] += 1
                     # End of a decimation loop. So, append accumulator value / decimation_factor  to the output string
-                    output_strings[ser_port] = output_strings[ser_port] + "{: 3.3f}, ".format(achan_accumulation_tables[ser_port][achan_number-1] * 10 / 32768 / decimation_factor)
+                    output_strings[ser_port] = output_strings[ser_port] + "{: 3.3f}, ".format(achan_accumulation_tables[ser_port][achan_numbers[ser_port]-1] * 10 / 32768 / decimation_factor)
 
-                elif (dec_count == 1) and (slist_pointer != 0):
+                elif (dec_counts[ser_port] == 1) and (slist_pointers[ser_port] != 0):
                     # Decimation loop finished and NOT the first slist position
                     # Two LSBs carry information only for first slist posiiton, which this isn't. So, ...
                     # Just add value to the accumulator
-                    achan_accumulation_tables[ser_port][achan_number] = result + achan_accumulation_tables[ser_port][achan_number]
-                    achan_number += 1
+                    achan_accumulation_tables[ser_port][achan_numbers[ser_port]] = result + achan_accumulation_tables[ser_port][achan_numbers[ser_port]]
+                    achan_numbers[ser_port] += 1
                     # End of a decimation loop. So, append accumulator value / decimation_factor  to the output string
-                    output_strings[ser_port] = output_strings[ser_port] + "{: 3.3f}, ".format(achan_accumulation_tables[ser_port][achan_number-1] * 10 / 32768 / decimation_factor)
+                    output_strings[ser_port] = output_strings[ser_port] + "{: 3.3f}, ".format(achan_accumulation_tables[ser_port][achan_numbers[ser_port]-1] * 10 / 32768 / decimation_factor)
 
-                elif (dec_count != 1) and (slist_pointer == 0):
+                elif (dec_counts[ser_port] != 1) and (slist_pointers[ser_port] == 0):
                     # Decimation loop NOT finished and first slist position
                     # Not the end of a decimation loop, but this is the first position in slist. So, ...
                     # Just strip two LSBs, preserving sign...
                     result = result >> 2
                     result = result << 2
                     # ...and add the value to the accumulator
-                    achan_accumulation_tables[ser_port][achan_number] = result + achan_accumulation_tables[ser_port][achan_number]
-                    achan_number += 1
+                    achan_accumulation_tables[ser_port][achan_numbers[ser_port]] = result + achan_accumulation_tables[ser_port][achan_numbers[ser_port]]
+                    achan_numbers[ser_port] += 1
                 else:
                     # Decimation loop NOT finished and NOT first slist position
                     # Nothing to do except add the value to the accumlator
-                    achan_accumulation_tables[ser_port][achan_number] = result + achan_accumulation_tables[ser_port][achan_number]
-                    achan_number += 1
+                    achan_accumulation_tables[ser_port][achan_numbers[ser_port]] = result + achan_accumulation_tables[ser_port][achan_numbers[ser_port]]
+                    achan_numbers[ser_port] += 1
 
                 # Get the next position in slist
-                slist_pointer += 1
+                slist_pointers[ser_port] += 1
 
-                if (slist_pointer + 1) > (len(slist)):
+                if (slist_pointers[ser_port] + 1) > (len(slist)):
                     # End of a pass through slist items
-                    if dec_count == 1:
+                    if dec_counts[ser_port] == 1:
                         # Get here if decimation loop has finished
-                        dec_count = decimation_factor
+                        dec_counts[ser_port] = decimation_factor
                         # Reset analog channel accumulators to zero
                         achan_accumulation_tables[ser_port] = [0] * len(achan_accumulation_tables[ser_port])
                         # Append digital inputs to output string
                         output_strings[ser_port] = output_strings[ser_port] + "{: 3d}, ".format(dig_in)
-                        print(output_strings) # DEBUG
+                        # print(output_strings) # DEBUG
                         print("Device: {}   -   ".format(ser_ports[ser_port].port), end='')
                         # print(output_strings[ser_port].rstrip(", ") + "           ", end="\r")
                         print(output_strings[ser_port].rstrip(", ") + "           ")
+                        if ser_port == len(ser_ports) - 1:
+                            print()
                         output_strings[ser_port] = ""
                     else:
-                        dec_count -= 1             
-                    slist_pointer = 0
-                    achan_number = 0
+                        dec_counts[ser_port] -= 1             
+                    slist_pointers[ser_port] = 0
+                    achan_numbers[ser_port] = 0
 
 # ser_ports[ser_port].close()
 SystemExit
