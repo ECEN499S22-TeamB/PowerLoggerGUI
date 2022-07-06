71d8900a00142b64722ab8e15739330a750f76dc
Jarmstrong42 <arm05006@byui.edu>
2022-07-06 12:40:56 -0600

    Update code

--- Joshua Armstrong/Dashboard.py
+++ Joshua Armstrong/Dashboard.py
@@ -141,7 +141,7 @@ def log_history(i=0):
     global lbl_output #
 
     # Update the listbox
-    daq_list.append(f"ERROR{i}\t35\n") # DEBUG
+    daq_list.append(f"ERROR{i}\n") # DEBUG
     i += 1 # DEBUG
     if not daq_details:
         daq_details = tk.StringVar(value=daq_list)
