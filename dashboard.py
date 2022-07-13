02b82c8652f6880b299e811116b46732b1915742
AUSTIN HILDERBRAND <rts.things@gmail.com>
2022-07-13 14:45:51 -0600

    Added file header to dashboard.py.

--- dashboard.py
+++ dashboard.py
@@ -1,3 +1,48 @@
+"""
+dashboard.py
+This script launches the dashboard
+Authors:    Joshua Armstrong
+            Austin Hilderbrand
+"""
+
+"""
+ECEN 499 Senior Project
+Instructor: Kevin Smith
+    208-496-7612
+    smithk@byui.edu
+
+Team B - Radiation Test Solutions
+    Aaron Mears (team lead)
+        509-596-8539
+        amears@radiationtestsolutions.com
+    Austin Hilderbrand
+        256-916-8954
+        austinlang748@gmail.com
+    Nathan Taylor
+        775-397-8139
+        tay18060@byui.edu
+    Joshua Armstrong
+        208-406-8689
+        arm05006@byui.edu
+"""
+
+"""
+GitHub tips -------------------------------------------------------------
+To upload changes
+    git add .   (stage changes - LOCAL)
+    git commit  (save staged changes - LOCAL)
+    git push    (upload saved changes to remote repo - LOCAL-->REMOTE)
+    
+To receive Changes
+    git pull    (download remote changes - REMOTE-->LOCAL)
+    
+To change branches
+    git checkout branchName
+
+To check status/verify branch
+    git status
+"""
+
 import tkinter as tk
 import tkinter.ttk as ttk
 import os
